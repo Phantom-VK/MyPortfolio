be06c3de2ae2f88280b3a4c8df86c7cbccea0d6b
VikramadityaKhupse <2022bit052@sggs.ac.in>
2024-11-06 18:43:44 +0530

    Added gradient shadow to cards.

--- site/src/jsMain/kotlin/com/vikramaditya/portfolio/styles/GoogleCardStyle.kt
+++ site/src/jsMain/kotlin/com/vikramaditya/portfolio/styles/GoogleCardStyle.kt
@@ -2,53 +2,63 @@ package com.vikramaditya.portfolio.styles
 
 import com.varabyte.kobweb.compose.css.BoxShadow
 import com.varabyte.kobweb.compose.css.Transition
-import com.varabyte.kobweb.compose.css.backgroundColor
-import com.varabyte.kobweb.compose.css.functions.linearGradient
+import com.varabyte.kobweb.compose.css.functions.LinearGradient
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.graphics.Colors
 import com.varabyte.kobweb.compose.ui.modifiers.background
-import com.varabyte.kobweb.compose.ui.modifiers.backgroundColor
-import com.varabyte.kobweb.compose.ui.modifiers.backgroundImage
 import com.varabyte.kobweb.compose.ui.modifiers.borderRadius
 import com.varabyte.kobweb.compose.ui.modifiers.boxShadow
-import com.varabyte.kobweb.compose.ui.modifiers.color
-import com.varabyte.kobweb.compose.ui.modifiers.onMouseEnter
-import com.varabyte.kobweb.compose.ui.modifiers.padding
 import com.varabyte.kobweb.compose.ui.modifiers.transform
 import com.varabyte.kobweb.compose.ui.modifiers.transition
 import com.varabyte.kobweb.silk.style.CssStyle
 import com.varabyte.kobweb.silk.style.selectors.hover
 import com.vikramaditya.portfolio.utils.Res
-import org.jetbrains.compose.web.css.deg
 import org.jetbrains.compose.web.css.ms
-import org.jetbrains.compose.web.css.percent
 import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.css.rgba
 
 val GoogleCardStyle = CssStyle {
-
-
     base {
         Modifier
             .background(if (colorMode.isLight) Colors.White else Colors.Black)
             .borderRadius(12.px)
             .boxShadow(BoxShadow.of(0.px, 4.px, 8.px, color = rgba(0, 0, 0, 0.1)))
             .transition(Transition.of("all", 300.ms))
-
     }
 
-    hover{
+    hover {
         Modifier
-            .boxShadow(BoxShadow.of(0.px, 8.px, 16.px, color = rgba(0, 0, 0, 0.2)))
-            .transform { scale(1.02) }
-            .color(Colors.White)
-            .backgroundImage(
-                linearGradient(45.deg) {
-                    add(Res.Theme.GoogleBlue.color, 0.percent)
-                    add(Res.Theme.GoogleRed.color, 33.percent)
-                    add(Res.Theme.GoogleYellow.color, 66.percent)
-                    add(Res.Theme.GoogleGreen.color)
-                }
+            .boxShadow(
+                // Google Blue shadow
+                BoxShadow.of(
+                    offsetX = (-4).px,
+                    offsetY = 4.px,
+                    blurRadius = 8.px,
+                    color = Res.Theme.GoogleBlue.color
+                ),
+                // Google Red shadow
+                BoxShadow.of(
+                    offsetX = 4.px,
+                    offsetY = 4.px,
+                    blurRadius = 8.px,
+                    color = Res.Theme.GoogleRed.color
+                ),
+                // Google Yellow shadow
+                BoxShadow.of(
+                    offsetX = 4.px,
+                    offsetY = (-4).px,
+                    blurRadius = 8.px,
+                    color = Res.Theme.GoogleYellow.color
+                ),
+                // Google Green shadow
+                BoxShadow.of(
+                    offsetX = (-4).px,
+                    offsetY = (-4).px,
+                    blurRadius = 8.px,
+                    color = Res.Theme.GoogleGreen.color
+                )
             )
+            .transform { scale(1.02) }
+            .borderRadius(12.px)
     }
 }
