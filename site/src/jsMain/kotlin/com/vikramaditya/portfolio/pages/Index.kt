a349e16af91f8a394f24f09ae3bc2c5f908e615a
Phantom-VK <vikramadityakhupse@gmail.com>
2025-05-17 13:54:01 +0530

    Added one more 3d cube

--- site/src/jsMain/kotlin/com/vikramaditya/portfolio/pages/Index.kt
+++ site/src/jsMain/kotlin/com/vikramaditya/portfolio/pages/Index.kt
@@ -1,30 +1,34 @@
 package com.vikramaditya.portfolio.pages
 
+
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Column
-import com.varabyte.kobweb.compose.foundation.layout.Spacer
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.alignContent
+import com.varabyte.kobweb.compose.ui.modifiers.alignSelf
 import com.varabyte.kobweb.compose.ui.modifiers.color
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.padding
 import com.varabyte.kobweb.compose.ui.modifiers.zIndex
 import com.varabyte.kobweb.core.Page
+import com.varabyte.kobweb.silk.components.layout.SimpleGrid
+import com.varabyte.kobweb.silk.components.layout.numColumns
 import com.varabyte.kobweb.silk.theme.breakpoint.rememberBreakpoint
 import com.varabyte.kobweb.silk.theme.colors.ColorMode
-import com.vikramaditya.portfolio.components.ProjectCard
 import com.vikramaditya.portfolio.layouts.PageLayout
-import com.vikramaditya.portfolio.sections.AboutMe
-import com.vikramaditya.portfolio.sections.MySkillsSection
-import com.vikramaditya.portfolio.sections.ProfileCard
-import com.vikramaditya.portfolio.sections.ProjectSection
-import com.vikramaditya.portfolio.sections.WhatIDo
+import com.vikramaditya.portfolio.sections.*
 import com.vikramaditya.portfolio.utils.Res
 import com.vikramaditya.portfolio.widgets.Cube3D
 import com.vikramaditya.portfolio.widgets.SectionTitle
-import com.vikramaditya.portfolio.widgets.HeaderItem
+import org.jetbrains.compose.web.css.AlignContent
+import org.jetbrains.compose.web.css.AlignSelf
+import org.jetbrains.compose.web.css.percent
+import org.jetbrains.compose.web.css.px
 
 
 @Page
@@ -60,7 +64,15 @@ fun HomePage() {
             SectionTitle("My Skills", id = "skills")
             MySkillsSection()
             SectionTitle("Tech Stack", id = "tech-stack")
-            Cube3D()
+            SimpleGrid(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(left = 350.px),
+                numColumns = numColumns(base = 1, lg = 2)
+            ){
+                Cube3D()
+                Cube3D()
+            }
             SectionTitle("Project", id = "projects")
             ProjectSection()
 
