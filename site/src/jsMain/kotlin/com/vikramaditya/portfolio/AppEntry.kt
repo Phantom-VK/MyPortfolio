7c1982f44dd2fe547a07f0820175f3b0329b3949
Phantom-VK <vikramaditya.khupse@bettrhq.com>
2026-05-24 13:07:09 +0530

    Made Dark theme as default theme

--- site/src/jsMain/kotlin/com/vikramaditya/portfolio/AppEntry.kt
+++ site/src/jsMain/kotlin/com/vikramaditya/portfolio/AppEntry.kt
@@ -3,6 +3,14 @@ package com.vikramaditya.portfolio
 import androidx.compose.runtime.Composable
 import com.varabyte.kobweb.core.App
 import com.varabyte.kobweb.silk.SilkApp
+import com.varabyte.kobweb.silk.init.InitSilk
+import com.varabyte.kobweb.silk.init.InitSilkContext
+import com.varabyte.kobweb.silk.theme.colors.ColorMode
+
+@InitSilk
+fun setInitialColorMode(ctx: InitSilkContext) {
+    ctx.config.initialColorMode = ColorMode.DARK
+}
 
 /**
  * Root entry point required by Kobweb. It boots Silk (the UI toolkit) and lets
